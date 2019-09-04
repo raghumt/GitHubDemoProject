13bd54d22fea432bec7792e21c3b1080b8802033
raghu <raghu@Raghu-PC>
2019-09-04 16:58:13 +0530

    first commit

---
+++ src/GitHubdemo.java
@@ -0,0 +1,17 @@
+
+public class GitHubdemo {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String s="hungry";
+		if(s=="yes")
+		{
+			System.out.println("eat somethinf");
+		}
+		else
+		{
+			System.out.println("work continuotion");
+		}
+	}
+
+}
